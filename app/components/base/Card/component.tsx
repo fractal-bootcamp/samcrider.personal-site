b73bc3a1b44c585a8300c0cc852c5c5f9f2e5420
SamuelRCrider <sam.crider@aol.com>
2024-06-03 13:12:37 -0400

    styled homepage

--- app/components/base/Card/component.tsx
+++ app/components/base/Card/component.tsx
@@ -1,19 +1,40 @@
-const Card = () => {
+import { Fields } from "./types";
+
+const Card = ({ image, text, link, links }: Fields) => {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure className="px-10 pt-10">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-          alt="Shoes"
-          className="rounded-xl"
-        />
+        {image ? (
+          <img
+            src={image}
+            alt="Shoes"
+            className="rounded-xl"
+            width={100}
+            height={100}
+          />
+        ) : null}
       </figure>
       <div className="card-body items-center text-center">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions">
-          <button className="btn btn-primary">Buy Now</button>
-        </div>
+        {text ? <h2 className="card-title">{text.title}</h2> : null}
+        {text ? <p>{text.text}</p> : null}
+        {link ? (
+          <div className="card-actions">
+            <a href={link} className="btn btn-primary">
+              Check It Out
+            </a>
+          </div>
+        ) : null}
+        {links ? (
+          <div className="card-actions">
+            {links.map((l, index) => {
+              return (
+                <a key={index} href={l.link}>
+                  {l.name}
+                </a>
+              );
+            })}
+          </div>
+        ) : null}
       </div>
     </div>
   );
--- app/components/pages/Home/component.tsx
+++ app/components/pages/Home/component.tsx
@@ -1,13 +1,35 @@
 import Card from "~/components/base/Card/component";
 import ProjectTimeLine from "~/components/compound/ProjectTimeLine";
 
+import s from "./styles.module.css";
+
 const Component = () => {
   return (
-    <div>
-      <div>
-        <Card />
-        <Card />
-        <Card />
+    <div className={s.homeContainer}>
+      <div className={s.cardContainer}>
+        <Card
+          image="/criderSelfie.PNG"
+          text={{ title: "ABOUT", text: "click the button to learn about me" }}
+          link="/about"
+        />
+        <Card
+          image="/thoughts.png"
+          text={{
+            title: "THOUGHTS",
+            text: "click the button to learn what I think about things",
+          }}
+          link="/thoughts"
+        />
+        <Card
+          text={{ title: "LINKS", text: "Here I am on the internet" }}
+          links={[
+            { name: "GitHub", link: "https://github.com/SamuelRCrider" },
+            {
+              name: "LinkedIn",
+              link: "https://www.linkedin.com/in/samuel-crider-7b0695205/",
+            },
+          ]}
+        />
       </div>
       <ProjectTimeLine />
     </div>
